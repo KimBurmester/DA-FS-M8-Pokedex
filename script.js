fc19388d3f8470fdba57af3664c7584ad32f7416
Kim Burmester <Kim.Burmester@mailbox.org>
2025-06-15 12:41:49 +0200

    style+fix: poke-cards fixed

--- script.js
+++ script.js
@@ -24,21 +24,43 @@
         document.getElementById('pokemon-card').innerHTML = `<p style="color:red;">${error.message}</p>`;
       }
     } */
+const typeColors = {
+  grass: 'rgb(168, 230, 162)',       // #A8E6A2
+  fire: 'rgb(255, 167, 86)',         // #FFA756
+  water: 'rgb(88, 171, 246)',        // #58ABF6
+  electric: 'rgb(251, 226, 115)',    // #FBE273
+  bug: 'rgb(195, 206, 117)',         // #C3CE75
+  normal: 'rgb(190, 190, 190)',      // #BEBEBE
+  poison: 'rgb(193, 131, 193)',      // #C183C1
+  ground: 'rgb(230, 192, 123)',      // #E6C07B
+  fairy: 'rgb(244, 189, 201)',       // #F4BDC9
+  fighting: 'rgb(225, 127, 126)',    // #E17F7E
+  psychic: 'rgb(250, 146, 178)',     // #FA92B2
+  rock: 'rgb(213, 213, 212)',        // #D5D5D4
+  ghost: 'rgb(120, 125, 156)',       // #787D9C
+  ice: 'rgb(180, 241, 241)',         // #B4F1F1
+  dragon: 'rgb(162, 124, 245)',      // #A27CF5
+  dark: 'rgb(169, 169, 169)',        // #A9A9A9
+  steel: 'rgb(204, 204, 222)',       // #CCCCDE
+  flying: 'rgb(168, 207, 255)'       // #A8CFFF
+};
+
 
     async function loadInitialPokemon() {
       const container = document.getElementById('pokemon-card');
       container.innerHTML = '';
-
       const limit = 40;
       const response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${limit}`);
       const data = await response.json();
-
       for (const pokemon of data.results) {
         const pokeData = await fetch(pokemon.url).then(res => res.json());
-
+        const type = pokeData.types[0].type.name;
+        const color = typeColors[type] || '#F0F0F0';
         const div = document.createElement('div');
+        div.className = 'pokemon-info box-shadow-bottom';
+        div.style.backgroundColor = color;
         div.innerHTML = `
-          <div class="pokemon-info box-shadow-bottom"><h3>${pokeData.name.toUpperCase()}</h3>
+          <div><h3>${pokeData.name.toUpperCase()}</h3>
           <p><strong>ID:</strong> ${pokeData.id}</p>
           <img src="${pokeData.sprites.front_default}" alt="${pokeData.name}">
           <p><strong>Typen:</strong> ${pokeData.types.map(t => t.type.name).join(', ')}</p>
@@ -49,3 +71,7 @@
     }
 /*           <p><strong>Größe:</strong> ${pokeData.height}</p>
           <p><strong>Gewicht:</strong> ${pokeData.weight}</p> */
+
+
+
+
